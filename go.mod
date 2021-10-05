2ab6801c938ec77fd6049cd532fe8b29bf14a178
root <locla@lc.loc>
2021-10-05 12:56:32 +0300

    added new resource feature_docker_support

--- go.mod
+++ go.mod
@@ -2,17 +2,16 @@ module github.com/cvbarros/terraform-provider-teamcity
 
 go 1.13
 
-replace (
-	github.com/cvbarros/go-teamcity  => ./go-teamcity
-)
+replace github.com/cvbarros/go-teamcity => ./go-teamcity
 
 require (
 	github.com/cvbarros/go-teamcity v1.1.0
 	github.com/dghubble/sling v1.3.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
-	github.com/hashicorp/terraform-plugin-sdk v1.9.0
+	github.com/hashicorp/terraform-plugin-sdk v1.17.2
+	github.com/hashicorp/terraform-plugin-test v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.8 // indirect
 	github.com/motemen/go-nuts v0.0.0-20190725124253-1d2432db96b0 // indirect
-	github.com/ulikunitz/xz v0.5.6 // indirect
 	github.com/vmihailenco/msgpack v4.0.4+incompatible // indirect
+	google.golang.org/grpc v1.32.0 // indirect
 )
---
+++ teamcity/resource_feature_docker_support.go
@@ -0,0 +1,115 @@
+package teamcity
+
+import (
+	"fmt"
+	api "github.com/cvbarros/go-teamcity/teamcity"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func resourceFeatureDockerSupport() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceFeatureDockerSupportCreate,
+		Read:   resourceFeatureDockerSupportRead,
+		Delete: resourceFeatureDockerSupportDelete,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"build_config_id": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"docker_registry": {
+				Type:        schema.TypeString,
+				Required:    true,
+				ForceNew:    true,
+				Description: "Foreign key of Docker Registry Connection",
+			},
+			"cleanup": {
+				Type:        schema.TypeBool,
+				Required:    true,
+				ForceNew:    true,
+				Description: "On server clean-up, delete pushed Docker images from registry",
+			},
+		},
+	}
+}
+
+func resourceFeatureDockerSupportCreate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*api.Client)
+	var buildConfigID string
+
+	if v, ok := d.GetOk("build_config_id"); ok {
+		buildConfigID = v.(string)
+	}
+
+	// validates the Build Configuration exists
+	if _, err := client.BuildTypes.GetByID(buildConfigID); err != nil {
+		return fmt.Errorf("invalid build_config_id '%s' - Build configuration does not exist", buildConfigID)
+	}
+
+	srv := client.BuildFeatureService(buildConfigID)
+
+	dt, err := buildDockerSupport(d)
+	if err != nil {
+		return err
+	}
+	out, err := srv.Create(dt)
+
+	if err != nil {
+		return err
+	}
+
+	d.SetId(out.ID())
+	return resourceFeatureDockerSupportRead(d, meta)
+}
+
+func resourceFeatureDockerSupportRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*api.Client).BuildFeatureService(d.Get("build_config_id").(string))
+
+	dt, err := getBuildFeatureDockerSupport(client, d.Id())
+	if err != nil {
+		return err
+	}
+
+	if err := d.Set("build_config_id", dt.BuildTypeID()); err != nil {
+		return err
+	}
+
+	opt := dt.Options.(*api.DockerSupportOptions)
+
+	m := make(map[string]interface{})
+	m["docker_registry"] = opt.Login2registry
+	m["cleanupPushed"] = opt.CleanupPushed
+
+	return err
+}
+
+func resourceFeatureDockerSupportDelete(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*api.Client)
+	svr := client.BuildFeatureService(d.Get("build_config_id").(string))
+
+	return svr.Delete(d.Id())
+}
+
+func buildDockerSupport(d *schema.ResourceData) (api.BuildFeature, error) {
+	var opt api.DockerSupportOptions
+	registry := d.Get("docker_registry").(string)
+	cleanup := d.Get("cleanup").(bool)
+
+	opt = api.NewDockerSupportOptions(registry, cleanup)
+
+	return api.NewFeatureDockerSupport(opt)
+}
+
+func getBuildFeatureDockerSupport(c *api.BuildFeatureService, id string) (*api.FeatureDockerSupport, error) {
+	dt, err := c.GetByID(id)
+	if err != nil {
+		return nil, err
+	}
+
+	fcsp := dt.(*api.FeatureDockerSupport)
+	return fcsp, nil
+}
